9ae64ecc3bdd984eb712220982953060b77d24ca
Andrey Yakovlev <andrey.e.yakovlev@megafon.ru>
2021-07-06 01:45:43 +0300

    Introduce EdgeInsetLabel

---
+++ TastyLife/core/customViews/EdgeInsetLabel.swift
@@ -0,0 +1,24 @@
+//
+//  EdgeInsetLabel.swift
+//  TastyLife
+//
+//  Created by Андрей Яковлев on 06.07.2021.
+//
+
+import UIKit
+
+class EdgeInsetLabel: UILabel {
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
+}
